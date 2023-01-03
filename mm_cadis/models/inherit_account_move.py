400719ce29b92e7934fdf248a5e920c657c4c31e
Developers-mit-mut <ulisesjimenez@mit-mut.com>
2023-01-03 13:05:39 -0600

    Ver 1.0.22

--- mm_cadis/models/inherit_account_move.py
+++ mm_cadis/models/inherit_account_move.py
@@ -1,9 +1,14 @@
+
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from lxml import etree, objectify
+from lxml.objectify import fromstring
+from odoo import api, fields, models, _, tools, release
 
-
-class AccountMove(models.Model):
+class InheritAccount(models.Model):
     _inherit = 'account.move'
+    _description = 'inherit account'
 
-    mm_taxex = fields.Many2one('res.partner', string='TAX ID')
+    child_parent = fields.Many2many('res.partner', string='mm_taxe')
+    
