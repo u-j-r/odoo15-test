b680a4d765ebd87b2af95be8640b4e2fea1c6552
Developers-mit-mut <ulisesjimenez@mit-mut.com>
2023-01-02 23:44:58 -0600

    Ver 1.0.2

--- mm_cadis/models/inherit_account_move.py
+++ mm_cadis/models/inherit_account_move.py
@@ -6,4 +6,4 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    mm_taxe = fields.Many2one('res.partner', string='TAX ID')
+    mm_taxex = fields.Many2one('res.partner', string='TAX ID')
